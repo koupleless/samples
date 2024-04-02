a19675c89fa79bcb6c9a3eaa73694e6ac51d6e47
CodeNoobKing <67936979+CodeNoobKing@users.noreply.github.com>
2024-04-02 21:29:56 +0800

    <fix>

---
+++ springboot-samples/web/tomcat/tomcat-web-integration-test/src/main/java/IntergrationTest.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2024 All Rights Reserved.
+ */
+
+/**
+ * @author gouzhendong.gzd
+ * @version $Id: IntergrationTest, v 0.1 2024-04-02 21:29 gouzhendong.gzd Exp $
+ */
+// just a place holder for ci
+public interface IntergrationTest {
+}
