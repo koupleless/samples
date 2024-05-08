f1876a0d9a295351cd5e943ab78b378fe8b427c9
CodeNoobKing <67936979+CodeNoobKing@users.noreply.github.com>
2024-05-08 17:59:36 +0800

    <ci> mongo integration test

---
+++ springboot-samples/db/mongo/mongo-integration-test/src/test/java/com/alipay/sofa/koupleless/integration/MongoIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.alipay.sofa.koupleless.integration;
+
+import com.alipay.sofa.db.base.BaseApplication;
+import com.alipay.sofa.db.biz1.Biz1Application;
+import com.alipay.sofa.db.biz2.Biz2Application;
+import com.alipay.sofa.koupleless.test.suite.spring.model.BaseSpringTestConfig;
+import com.alipay.sofa.koupleless.test.suite.spring.model.BizSpringTestConfig;
+import com.alipay.sofa.koupleless.test.suite.spring.model.MultiSpringTestConfig;
+import com.alipay.sofa.koupleless.test.suite.spring.multi.TestMultiSpringApplication;
+import com.google.common.collect.Lists;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MongoIntegrationTest {
+    @BeforeClass
+    public static void setUp() {
+        TestMultiSpringApplication multiApp = new TestMultiSpringApplication(
+                MultiSpringTestConfig
+                        .builder()
+                        .baseConfig(BaseSpringTestConfig
+                                .builder()
+                                .mainClass(BaseApplication.class)
+                                .build())
+                        .bizConfigs(Lists.newArrayList(
+                                BizSpringTestConfig
+                                        .builder()
+                                        .bizName("biz1")
+                                        .mainClass(Biz1Application.class)
+                                        .build(),
+                                BizSpringTestConfig
+                                        .builder()
+                                        .bizName("biz2")
+                                        .mainClass(Biz2Application.class)
+                                        .build()
+                        ))
+                        .build()
+        );
+        multiApp.run();
+    }
+
+    @Test
+    public void test() {
+        System.out.println("hello world");
+    }
+}
