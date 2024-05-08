a3f12952df758cf31df0eed7077096c6aae26339
CodeNoobKing <67936979+CodeNoobKing@users.noreply.github.com>
2024-05-08 17:23:20 +0800

    <ci> mybatis integration test

---
+++ springboot-samples/db/mybatis/mybatis-integration-test/src/test/java/com/alipay/sofa/koupleless/integration/MyBatisIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.alipay.sofa.koupleless.integration;
+
+import com.alipay.sofa.base.BaseApplication;
+import com.alipay.sofa.biz1.Biz1Application;
+import com.alipay.sofa.koupleless.test.suite.spring.model.BaseSpringTestConfig;
+import com.alipay.sofa.koupleless.test.suite.spring.model.BizSpringTestConfig;
+import com.alipay.sofa.koupleless.test.suite.spring.model.MultiSpringTestConfig;
+import com.alipay.sofa.koupleless.test.suite.spring.multi.TestMultiSpringApplication;
+import com.google.common.collect.Lists;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MyBatisIntegrationTest {
+
+    @BeforeClass
+    public static void setUp() {
+        TestMultiSpringApplication multiApp = new TestMultiSpringApplication(
+                MultiSpringTestConfig
+                        .builder()
+                        .baseConfig(BaseSpringTestConfig
+                                .builder()
+                                .mainClass(BaseApplication.class)
+                                .build())
+                        .bizConfigs(Lists.newArrayList(
+                                BizSpringTestConfig
+                                        .builder()
+                                        .bizName("biz1")
+                                        .mainClass(Biz1Application.class)
+                                        .build()
+                        ))
+                        .build()
+        );
+        multiApp.run();
+    }
+
+    @Test
+    public void test() {
+        System.out.println("hello world");
+    }
+}
