3635d8070fcdf296b7e9b820f798de38a1df630a
CodeNoobKing <67936979+CodeNoobKing@users.noreply.github.com>
2024-05-23 14:20:42 +0800

    <fix> test

--- springboot-samples/web/webflux/webflux-integration-test/src/test/java/com/alipay/sofa/koupleless/integration/WebfluxIntegrationTest.java
+++ springboot-samples/web/webflux/webflux-integration-test/src/test/java/com/alipay/sofa/koupleless/integration/WebfluxIntegrationTest.java
@@ -20,7 +20,7 @@ import com.alipay.sofa.koupleless.test.suite.spring.model.BaseSpringTestConfig;
 import com.alipay.sofa.koupleless.test.suite.spring.model.BizSpringTestConfig;
 import com.alipay.sofa.koupleless.test.suite.spring.model.MultiSpringTestConfig;
 import com.alipay.sofa.koupleless.test.suite.spring.multi.TestMultiSpringApplication;
-import com.example.yuan.bizwebflux.BizwebfluxApplication;
+import com.example.yuan.bizwebflux.BizWebfluxApplication;
 import com.example.yuan.demowebflux.DemoWebfluxApplication;
 import com.google.common.collect.Lists;
 import org.junit.BeforeClass;
@@ -31,12 +31,12 @@ public class WebfluxIntegrationTest {
     @BeforeClass
     public static void setUp() {
         TestMultiSpringApplication multiApp = new TestMultiSpringApplication(MultiSpringTestConfig
-            .builder()
-            .baseConfig(
-                BaseSpringTestConfig.builder().mainClass(DemoWebfluxApplication.class).build())
-            .bizConfigs(
-                Lists.newArrayList(BizSpringTestConfig.builder().bizName("biz")
-                    .mainClass(BizwebfluxApplication.class).build())).build());
+                .builder()
+                .baseConfig(
+                        BaseSpringTestConfig.builder().mainClass(DemoWebfluxApplication.class).build())
+                .bizConfigs(
+                        Lists.newArrayList(BizSpringTestConfig.builder().bizName("biz")
+                                .mainClass(BizWebfluxApplication.class).build())).build());
         multiApp.run();
     }
 
