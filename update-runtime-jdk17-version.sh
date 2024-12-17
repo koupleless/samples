edbb9fdcba17658770f2577e4934be918dc7dc4a
lvjing2 <leojames.googol@gmail.com>
2024-12-17 20:57:36 +0800

    update update version shell

--- update-runtime-jdk17-version.sh
+++ update-runtime-jdk17-version.sh
@@ -4,9 +4,12 @@ DEST_VERSION=$1
 
 # 如果 DEST_VERSION 为空，则报错
 if [ -z "$DEST_VERSION" ]; then
-  echo "Error: SOURCE_VERSION and DEST_VERSION must be provided."
+  echo "Error: DEST_VERSION must be provided."
   exit 1
 fi
 
-# git grep -rn '2.2.12' | cat | awk -F: '{print $1}' | xargs -I {} sed -i '' 's/2.2.12/2.2.13/g' {}
-git grep -rn "<koupleless.runtime.version>2\..*</koupleless.runtime.version>" | cat | awk -F: '{print $1}' | xargs -I {} sed -i '' "s/\<koupleless.runtime.version\>2\..*\<\/koupleless.runtime.version\>/\<koupleless.runtime.version\>$1\<\/koupleless.runtime.version\>/g" {}
+if [[ "$(uname)" == "Darwin" ]]; then
+  grep -rn --include="*.xml" "<koupleless.runtime.version>2\..*</koupleless.runtime.version>" | awk -F: '{print $1}' | xargs -I {} sed -i '' "s/\<koupleless.runtime.version\>2\..*\<\/koupleless.runtime.version\>/\<koupleless.runtime.version\>$1\<\/koupleless.runtime.version\>/g" {}
+else
+  grep -rn --include="*.xml" "<koupleless.runtime.version>2\..*</koupleless.runtime.version>" | awk -F: '{print $1}' | xargs -I {} sed -i "s/\<koupleless.runtime.version\>2\..*\<\/koupleless.runtime.version\>/\<koupleless.runtime.version\>$1\<\/koupleless.runtime.version\>/g" {}
+fi
